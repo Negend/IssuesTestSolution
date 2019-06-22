7134d9679fd299107fdcec796843b959650fc18d
Nneji Nwakamma <nneji.2day@yahoo.co.uk>
2019-06-22 18:31:38 +0100

    Project setup

---
+++ IssuesTestSolution/Helpers/HttpRequests.cs
@@ -0,0 +1,36 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssuesTestSolution.Helpers
+{
+    public static class HttpRequests
+    {
+        public static RestClient client;
+        public static RestRequest restRequest;
+        public static string baseUrl = "";
+
+        public static RestClient SetUrl(string endpoint)
+        {
+            var url = Path.Combine(baseUrl, endpoint);
+            return client = new RestClient(url);
+
+        }
+
+        public static RestRequest CreateRequest()
+        {
+            restRequest = new RestRequest(Method.GET);
+            restRequest.AddHeader("Accept", "application/json");
+            return restRequest;
+        }
+
+        public static IRestResponse GetResponse()
+        {
+            return client.Execute(restRequest);
+        }
+    }
+}
